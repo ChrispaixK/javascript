d237c0ba1dfb15f8e410290f0bddaa2c33d1403c
Chrispaix KAZE <kchrispaixs@gmail.com>
2022-08-07 16:48:32 +0200

    Fix checking bug

--- Me/GuessMyNumber/script.js
+++ Me/GuessMyNumber/script.js
@@ -33,10 +33,9 @@ document.querySelector('.check').addEventListener('click', function () {
     // document.querySelector('.message').textContent = '🎉 Correct Number!';
     displayMessage('🎉 Correct Number!');
     document.querySelector('.number').textContent = secretNumber;
-
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
-
+    document.querySelector('.check').style.display='none'
     if (score > highscore) {
       highscore = score;
       document.querySelector('.highscore').textContent = highscore;
@@ -68,7 +67,7 @@ document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('.score').textContent = score;
   document.querySelector('.number').textContent = '?';
   document.querySelector('.guess').value = '';
-
+  document.querySelector('.check').style.display='block'
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').style.width = '15rem';
 });
