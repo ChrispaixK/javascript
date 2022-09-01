cb3388d87d2369a131e854cb0481c0b2479d43f7
Chrispaix KAZE <kchrispaixs@gmail.com>
2022-09-01 11:22:26 +0200

    lol

--- Me/thiskeyword/script.js
+++ Me/thiskeyword/script.js
@@ -3,9 +3,19 @@
 //this log a window object
 console.log(this);
 
-const calcAge= (birthYear)=> {
+const calcAge= function (birthYear) {
     console.log(2091 - birthYear);
+    // in regular function this pointing to undefined
     console.log(this);
 }
 
 calcAge(1996)
+
+const calcAgeArrow= (birthYear)=> {
+    console.log(2091 - birthYear);
+    // arroe functions point to their parent
+    //this will point on window Obj
+    console.log(this);
+}
+
+calcAgeArrow(1996)
